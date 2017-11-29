6e0dff6243577baff4c6cc77ad439c41e3adfb2f
metiner <metiner.kocak@cs.com.tr>
2017-11-29 15:57:08 +0200

    providers page v2

--- src/pages/provider/provider.ts
+++ src/pages/provider/provider.ts
@@ -15,6 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProviderPage {
 
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
