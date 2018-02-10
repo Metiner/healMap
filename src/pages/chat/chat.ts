c53b8a2be78d2b51d4afddf51b2fdeca0abe441c
metiner.kocak <metinerkck@gmail.com>
2018-02-10 21:18:14 +0300

    launch navigator implemented.

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {Content, IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 import {Provider} from "../../models/provider";
 import {HealMapLib} from "../../services/healMapLib";
+import {NgForm} from "@angular/forms";
 
 /**
  * Generated class for the ChatPage page.
@@ -17,6 +18,9 @@ import {HealMapLib} from "../../services/healMapLib";
 })
 export class ChatPage {
 
+  @ViewChild(Content) content:Content;
+
+
   sender:Provider;
   receiver:Provider;
   thread:any;
@@ -24,38 +28,73 @@ export class ChatPage {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              public healMapLib: HealMapLib) {
+              public healMapLib: HealMapLib,
+              public platform:Platform) {
+
+
 
       this.thread = this.navParams.data;
 
-      if(this.thread.sender_id.id == this.healMapLib.provider.user.id){
-        this.sender = this.thread.receiver_id;
-        this.receiver = this.healMapLib.provider;
-      }else{
-        this.sender = this.thread.sender_id;
-        this.receiver = this.thread.receiver_id;
-      }
+      this.platform.ready().then(()=>{
 
+        this.setSenderAndReceiver();
+        this.initializeMessagesQueue().then(()=>{
+          this.scrollToEndOfChat();
+        })
+      })
 
-
-      for(let i=0;i<this.thread.messages.length;i++){
-        if(this.thread.messages[i].user_id == this.healMapLib.provider.user.id){
-          this.thread.messages[i].isMessageFromCurrentUser = true;
-        }
-        else{
-          this.thread.messages[i].isMessageFromCurrentUser = false;
-        }
-
-      }
   }
 
-  async sendMessage(form){
+  setSenderAndReceiver(){
+
+    console.log("1");
+    if(this.thread.sender_id.id == this.healMapLib.provider.user.id){
+      this.sender = this.thread.receiver_id;
+      this.receiver = this.healMapLib.provider;
+    }else{
+      this.sender = this.thread.sender_id;
+      this.receiver = this.thread.receiver_id;
+    }
+  }
+
+  initializeMessagesQueue():Promise<boolean>{
+
+
+    for(let i=0;i<this.thread.json_body.messages.length;i++){
+
+      if(this.thread.json_body.messages[i].user_id == this.healMapLib.provider.user.id){
+        this.thread.json_body.messages[i].isMessageFromCurrentUser = true;
+      }
+      else{
+        this.thread.json_body.messages[i].isMessageFromCurrentUser = false;
+      }
+    }
+    return new Promise(resolve => {
+
+      resolve(true);
+    })
+  }
+
+  scrollToEndOfChat(){
+    console.log("4");
+    setTimeout(()=>{
+      this.content.scrollToBottom(1000);
+    },500)
+  }
+
+  async sendMessage(form:NgForm){
 
     await this.healMapLib.sendMessage(this.thread.id,form.value.messageBody).then(success=>{
-      this.thread.messages = success.json().json_body.messages;
+      this.thread.json_body.messages = success.json().json_body.messages;
+
     }).catch(error=>{
-      console.log(error);
+      });
+    await this.initializeMessagesQueue().then(success=>{
+
+      this.scrollToEndOfChat();
     });
+
+    form.resetForm();
 
   }
 
